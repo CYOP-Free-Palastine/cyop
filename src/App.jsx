bd8f77121c571fb66339d7ad4a634671ead1254d
ammoriddin <hakimjavoh@gmail.com>
2024-06-29 04:51:28 -0700

    CREATE: Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { routes } from './routes';
 import NavbarSection from '@containers/navbar';
+import Footer from '@containers/footer';
 
 function App() {
 
   return (
     <Router>
       <NavbarSection />
-      <Routes>
-        {routes.map((route, index) => (
-          <Route key={index} path={route.path} element={route.element} />
-        ))}
-      </Routes>
+      <main>
+        <Routes>
+          {routes.map((route, index) => (
+            <Route key={index} path={route.path} element={route.element} />
+          ))}
+        </Routes>
+      </main>
+      <Footer />
     </Router>
   )
 }

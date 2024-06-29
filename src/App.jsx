da6ed834932dcf177f8f3009a2a576b9cf762b61
ammoriddin <hakimjavoh@gmail.com>
2024-06-29 01:26:06 -0700

    CREATE: Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { routes } from './routes';
+import NavbarSection from '@containers/navbar';
 
 function App() {
 
   return (
     <Router>
+      <NavbarSection />
       <Routes>
         {routes.map((route, index) => (
           <Route key={index} path={route.path} element={route.element} />
---
+++ src/containers/links/inde.js
@@ -0,0 +1,22 @@
+export const navbarLinks = [
+    {
+        label: "Home",
+        path: '/'
+    },
+    {
+        label: "About",
+        path: '/about'
+    },
+    {
+        label: "Subscribes",
+        path: '/subscribes'
+    },
+    {
+        label: "Templates",
+        path: '/templates'
+    },
+    {
+        label: "Support",
+        path: '/support'
+    },
+]

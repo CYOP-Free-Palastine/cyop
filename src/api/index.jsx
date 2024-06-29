5f72b5db6f0d02a4a2529a19e1b4493dc656ecbd
ammoriddin <hakimjavoh@gmail.com>
2024-06-28 23:25:20 -0700

    Create react app and connect alias,react-router,tailwindcss,flowbite-react

---
+++ src/api/index.jsx
@@ -0,0 +1,1 @@
+export const api = "https://api.twitter.com"
---
+++ src/routes/index.jsx
@@ -0,0 +1,8 @@
+import Home from "@pages/home-page";
+
+export const routes = [
+    {
+        path: '/',
+        element: <Home />
+    }
+]

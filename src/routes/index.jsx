7d18f23f187fab3255045aa30af47434a4a7ecee
ammoriddin <hakimjavoh@gmail.com>
2024-06-30 03:03:38 -0700

    CREATE: Register-section

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,8 +1,13 @@
 import Home from "@pages/home-page";
+import Register from "@pages/register";
 
 export const routes = [
     {
         path: '/',
         element: <Home />
+    },
+    {
+        path: '/register',
+        element: <Register />
     }
 ]
